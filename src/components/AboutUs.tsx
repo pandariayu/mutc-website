7ff650fcaf4b10775c5fbf9ae41771e442cb475f
Jason <junquany@student.unimelb.edu.au>
2025-03-24 12:32:51 +1100

    Add achievement on 23/3

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -6,7 +6,7 @@ import {ArrowRight} from "lucide-react"
 export default function Page() {
     const awards = [
         {
-            title: "2XU Series 1 Elwood Triathlon",
+            title: "2XU Triathlon Series 2025 - Race 1 Elwood",
             achievement: "First Medal & 1st Place",
             details: "Ranking 1st place in the overall team relays out of 25 teams",
             date: "November, 2024",
@@ -18,6 +18,13 @@ export default function Page() {
             details: "Outstanding achievement in innovation and inclusion",
             date: "December, 2024",
             url: "https://sport.unimelb.edu.au/news/melbourne-plus-recognises-students-volunteering-in-sport"
+        },
+        {
+            title: "2XU Triathlon Series 2025 - Race 6 St Kilda",
+            achievement: "1st Place in Mixed Relay",
+            details: "Ranking 1st place in the mixed relays out of 13 teams",
+            date: "March, 2025",
+            url: "https://www.multisportaustralia.com.au/races/2xu-triathlon-series-2425-race-6-st-kilda-2025/events/4/results/individuals/2906"
         },
     ]
     return (
