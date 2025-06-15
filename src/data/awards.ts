d426771b005d35711c7a65ebcf3ab9fa17396caa
Jason <junquany@student.unimelb.edu.au>
2025-06-15 20:48:30 +1000

    build: fix building issue

---
+++ src/data/awards.ts
@@ -0,0 +1,24 @@
+﻿import { IAwards } from "@/types";
+
+export const awardsDetail :IAwards[] = [
+    {
+        title: "2XU Triathlon Series 2025 - Race 1 Elwood",
+        achievement: "First Medal & 1st Place",
+        details: "Ranking 1st place in the overall team relays out of 25 teams",
+        date: "November, 2024",
+        url: "https://www.multisportaustralia.com.au/races/2xu-triathlon-series-2425-race-1-elwood-2024/events/4/results/individuals/1459"
+    },
+    {
+        title: "MelbUniSports Recognition",
+        achievement: "Award of Excellence",
+        details: "Outstanding achievement in innovation and inclusion",
+        date: "December, 2024",
+        url: "https://sport.unimelb.edu.au/news/melbourne-plus-recognises-students-volunteering-in-sport"
+    },
+    {
+        title: "2XU Triathlon Series 2025 - Race 6 St Kilda",
+        achievement: "1st Place in Mixed Relay",
+        details: "Ranking 1st place in the mixed relays out of 13 teams",
+        date: "March, 2025",
+        url: "https://www.multisportaustralia.com.au/races/2xu-triathlon-series-2425-race-6-st-kilda-2025/events/4/results/individuals/2906"
+    }]
---
+++ src/data/trainingSessions.ts
@@ -0,0 +1,45 @@
+﻿import {ISessions} from "@/types";
+
+export const sessions : ISessions[] = [
+    {
+        image: "/images/Swim.jpg",
+        title: "Monday/Thursday Swim",
+        level: "All level",
+        location: "Beaureparies Center Swimming pool",
+        time: "Monday: 2:00 pm - 3:30 pm",
+        time2: "Thursday: 11:00 am - 12:30 pm",
+        content: "Train with Coach Chris",
+        weekday: [1,4],
+    },
+    {
+        image: "/images/IntRun.png",
+        title: "Wednesday Interval Running",
+        level: "Intermediate level",
+        location: "Rawlinson Track",
+        time: "7:30 pm - 8:15 pm ",
+        time2: "",
+        content: "Train with coordinator Lan and Balthazar",
+        weekday: 3,
+    },
+    {
+        image: "/images/Bike&Run.jpg",
+        title: "Friday Indoor Bike&Run",
+        level: "All level",
+        location: "Rawlinson Track",
+        time: "3:00 pm - 4:00 pm ",
+        time2: "",
+        content: "Bike then Run, Two Running paces (5:15 min/km & 6:00 min/km)",
+        weekday: 5,
+    },
+    {
+        image:
+            "/images/SocialRun.png",
+        title: "Saturday Social Run",
+        level: "Beginner level",
+        location: "Rawlinson Track",
+        time: "9:00 - 9:45 am",
+        time2: "",
+        content: "Parkville Easy Social Run at you pace, free coffee will be provided",
+        weekday: 6,
+    },
+]
